007d4afe9a1b3284b60ab7c8b07349a0a6025862
sreeparvathy13 <sreeparvathydotwibe@outlook.com>
2025-11-05 13:09:11 +0530

    ss

--- src/app/(frontend)/components/Header/Header.tsx
+++ src/app/(frontend)/components/Header/Header.tsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import logo from "../../../../../public/images/logo.png"
-import LanguageSwitcher from '../LanguageSwitcher '
+
+
+
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../../../public/images/logo.png";
 
 const Header = () => {
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className='header'>
+    <div className={`header ${isFixed ? "fixedtop" : ""}`}>
       <div className="containers">
         <div className="header-div">
           <div className="header-col1">
             <div className="header-logo">
               <Link href="/">
-                <Image
-                  src={logo}
-                  alt="logo"
-                  width={100}
-                  height={19}
-                />
+                <Image src={logo} alt="logo" width={100} height={19} />
               </Link>
             </div>
 
@@ -26,17 +39,19 @@ const Header = () => {
             </div>
           </div>
 
-
           <div className="header-col2">
-            <LanguageSwitcher />
-           
-            <Link href="/login" className='h-login'>Login</Link>
-            <Link href="/signup" className='h-btn'>Get Listed</Link>
+            {/* <LanguageSwitcher /> */}
+            <Link href="/login" className="h-login">
+              Login
+            </Link>
+            <Link href="/signup" className="h-btn">
+              Get Listed
+            </Link>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
