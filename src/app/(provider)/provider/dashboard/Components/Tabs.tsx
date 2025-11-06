1f144483b52d54c7abc9c641ab0a4f70e9fe6959
supportdotwibe <support@dotwibe.com>
2025-11-06 17:59:03 +0530

    up

--- src/app/(provider)/provider/dashboard/Components/Tabs.tsx
+++ src/app/(provider)/provider/dashboard/Components/Tabs.tsx
@@ -37,6 +37,7 @@ const Tabs = () => {
 
         
         <div className={`tab-panel ${activeTab === "companyinfo" ? "show" : ""}`}>
+          <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, voluptatum!</h2>
   <form className="settingsform">
             <div className="form-grp">
               <label>Company/Business Name</label>
@@ -117,93 +118,7 @@ const Tabs = () => {
             <button type="submit" className="btn get-sub">
               Save
             </button>
-          </form> <div className="form-grp">
-                    <label> Notes</label>
-                    <input
-                        type="text"
-                        name="title"
-                        placeholder="Enter title"
-                        required
-                    />
-                  </div>
-                <h4>Online Presence</h4>
-
-                  <div className="form-grp">
-                    <label> Website</label>
-                    <input
-                        type="text"
-                        name="title"
-                        placeholder="Enter title"
-                        required
-                    />
-                  </div>
-
-
-                    <div className="form-grp">
-                    <label> Facebook</label>
-                    <input
-                        type="text"
-                        name="title"
-                        placeholder="Enter title"
-                        required
-                    />
-                  </div>
-
-
-
-                    <div className="form-grp">
-                    <label> Instagram</label>
-                    <input
-                        type="text"
-                        name="title"
-                        placeholder="Enter title"
-                        required
-                    />
-                  </div>
-
-
-
-                    <div className="form-grp">
-                    <label> Other</label>
-                    <input
-                        type="text"
-                        name="title"
-                        placeholder="Enter title"
-                        required
-                    />
-                  </div>
-
-
-                <h4>Services</h4>
-
-                    <div className="form-grp">
-                    <label> Select services</label>
-                    <input
-                        type="text"
-                        name="title"
-                        placeholder="Enter title"
-                        required
-                    />
-                  </div>
-
-                        <button type="submit" className="btn get-sub" >
-save
-                        </button>
-
-                    {/* {message && (
-                    <p
-                        className={`message ${
-                        message.includes("✅")
-                            ? "success"
-                            : message.includes("⚠️")
-                            ? "warning"
-                            : "error"
-                        }`}
-                    >
-                        {message}
-                    </p>
-                    )} */}
-                </form>
+          </form>
         </div>
 
         <div
