cb99a89f5399d1ab8d2a41e906639f72fd145c3e
sreeparvathy13 <sreeparvathydotwibe@outlook.com>
2025-11-06 19:14:22 +0530

    ss

--- src/app/(directory)/service-provider-directory/page.tsx
+++ src/app/(directory)/service-provider-directory/page.tsx
@@ -12,7 +12,11 @@ const ServiceProviderDirectory = () => {
 
         <Intro />
 
-        <div className="house-div">
+
+
+        <div className="repeat-house-div">
+
+          <div className="house-div">
 
             <HouseCard 
         logo={logo1}   
@@ -25,14 +29,144 @@ const ServiceProviderDirectory = () => {
         </div>
 
 
-      
+
+        <div className="details">
+
+
+          <div className="det1">
+            <div className="d-row">
+              <div className="d-col">
+                <p><strong>Services</strong></p>
+                
+              </div>
+              <div className="d-col">
+                <p>Architectural Services / Construction</p>
+
+              </div>
+            </div>
+            <div className="d-row">
+              <div className="d-col">
+                <p><strong>Luxury type</strong></p>
+                
+              </div>
+              <div className="d-col">
+                <p>Modern, Futurism, Classic</p>
+
+              </div>
+            </div>
+
+          </div>
+
+
+
+          <div className="det1 det2">
+             <div className="d-row">
+              <div className="d-col">
+                <p><strong> Starting Budget</strong></p>
+                
+              </div>
+              <div className="d-col">
+                <p>$10,000</p>
+
+              </div>
+            </div>
+             <div className="d-row">
+              <div className="d-col">
+                <p><strong> Location</strong></p>
+                
+              </div>
+              <div className="d-col">
+                <p>Kuwait</p>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+        <ImageSlider />
+
+        </div>
+        <div className="repeat-house-div">
+
+          <div className="house-div">
+
+            <HouseCard 
+        logo={logo1}   
+        name="American House Improvements Inc."
+        description="LA’s Highly Rated, Award-Winning Construction & Renovation Firm"
+      />
+
+      <button className='detail-btn'>Details</button>
+
+        </div>
+
+
+
+        <div className="details">
+
+
+          <div className="det1">
+            <div className="d-row">
+              <div className="d-col">
+                <p><strong>Services</strong></p>
+                
+              </div>
+              <div className="d-col">
+                <p>Architectural Services / Construction</p>
+
+              </div>
+            </div>
+            <div className="d-row">
+              <div className="d-col">
+                <p><strong>Luxury type</strong></p>
+                
+              </div>
+              <div className="d-col">
+                <p>Modern, Futurism, Classic</p>
+
+              </div>
+            </div>
+
+          </div>
+
+
+
+          <div className="det1 det2">
+             <div className="d-row">
+              <div className="d-col">
+                <p><strong> Starting Budget</strong></p>
+                
+              </div>
+              <div className="d-col">
+                <p>$10,000</p>
+
+              </div>
+            </div>
+             <div className="d-row">
+              <div className="d-col">
+                <p><strong> Location</strong></p>
+                
+              </div>
+              <div className="d-col">
+                <p>Kuwait</p>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+        <ImageSlider />
+
+        </div>
+
+        
+
 
      
-
-
-
-        <ImageSlider />
-        <ImageSlider />
       
     </div>
   )
