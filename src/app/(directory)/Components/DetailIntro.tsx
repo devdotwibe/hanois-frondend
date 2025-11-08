3c292b528776148396bd51d5d190cb2a80f7096c
supportdotwibe <support@dotwibe.com>
2025-11-08 17:47:16 +0530

    up

--- src/app/(directory)/Components/DetailIntro.tsx
+++ src/app/(directory)/Components/DetailIntro.tsx
@@ -1,26 +1,31 @@
+// app/(directory)/Components/DetailIntro.jsx
 import React from 'react'
 import DetailCard from '@/app/(directory)/Components/DetailCard'
-import logo1 from "../../../../public/images/ahi-logo.jpg"; 
+import Image from 'next/image'
 
+const DetailIntro = ({ name, description, image }) => {
+  // If you want to keep a default local logo, import it and use as fallback
+  // import logoFallback from '../../../../public/images/ahi-logo.jpg'
+  // const logoSrc = image ?? logoFallback
 
-
-const DetailIntro = () => {
   return (
-      <div className="detail-page-intro">
-        <div className="">
-             <DetailCard 
-         logo={logo1}   
-        name="American House Improvements Inc."
-        description="LA’s Highly Rated, Award-Winning Construction & Renovation Firm"
+    <div className="detail-page-intro">
+      <div className="">
+        <DetailCard
+          // DetailCard should accept logo, name, description. If not, update it similarly.
+          logo={image}
+          name={name ?? 'Unknown Provider'}
+          description={description ?? ''}
         />
 
-        </div>
-
-
-       
-
-        </div>
-
+        {/* If DetailCard doesn't handle an image prop, you can also render here: */}
+        {/* {image && (
+          <div className="provider-image">
+            <Image src={image} alt={name ?? 'provider image'} width={120} height={120} />
+          </div>
+        )} */}
+      </div>
+    </div>
   )
 }
 
