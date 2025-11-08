ac4246df0e4a2efd224719971507afa0438d7f4f
supportdotwibe <support@dotwibe.com>
2025-11-08 18:19:07 +0530

    uo

--- src/app/(directory)/service-provider-directory/(pages)/details/page.tsx
+++ src/app/(directory)/service-provider-directory/(pages)/details/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import DetailIntro from '@/app/(directory)/Components/DetailIntro';
 import AboutContainer from '../../Components/AboutContainer';
