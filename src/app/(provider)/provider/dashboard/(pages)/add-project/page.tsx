1e9996d5e4bdd5fb0d651a10055ff75a100fd0f9
supportdotwibe <support@dotwibe.com>
2025-11-10 12:25:06 +0530

    up

--- src/app/(provider)/provider/dashboard/(pages)/add-project/page.tsx
+++ src/app/(provider)/provider/dashboard/(pages)/add-project/page.tsx
@@ -1,29 +1,31 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import uploadIcon from "../../../../../../../public/images/upload.svg"; 
+import uploadIcon from "../../../../../../../public/images/upload.svg";
+import HouseOuter from "../../Components/HouseOuter"; // <-- add this import
 
 const UploadBox = () => {
   return (
-    
-    <div className="upload-box">
-      <div className="upload-content">
-        {/* ✅ Replace UploadCloud with your image */}
-        <Image 
-          src={uploadIcon} 
-          alt="Upload Icon" 
-          width={40} 
-          height={40} 
-          className="upload-icon"
-        />
+    <>
+      <HouseOuter />           {/* <-- render it here, outside the upload-box */}
+      <div className="upload-box">
+        <div className="upload-content">
+          <Image
+            src={uploadIcon}
+            alt="Upload Icon"
+            width={40}
+            height={40}
+            className="upload-icon"
+          />
 
-        <h3>Upload an image</h3>
-        <p>Browse your files To upload Document</p>
-        <span>Supported Formats: JPEG, PNG</span>
+          <h3>Upload an image</h3>
+          <p>Browse your files To upload Document</p>
+          <span>Supported Formats: JPEG, PNG</span>
+        </div>
+
+        <input type="file" accept="image/*" className="upload-input hidden" />
       </div>
-
-      <input type="file" accept="image/*" className="upload-input hidden" />
-    </div>
+    </>
   );
 };
 
