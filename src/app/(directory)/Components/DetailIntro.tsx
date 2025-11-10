b3a77022d22733167ebc512a823e61f6e754253b
supportdotwibe <support@dotwibe.com>
2025-11-10 19:18:42 +0530

    up

--- src/app/(directory)/Components/DetailIntro.tsx
+++ src/app/(directory)/Components/DetailIntro.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import DetailCard from '@/app/(directory)/Components/DetailCard';
 import defaultLogo from "../../../../public/images/ahi-logo.jpg"; 
+import { IMG_URL } from "@/config";
 
 const DetailIntro = ({ provider }) => {
   const name = provider?.name || "Unknown Provider";
-  const description = provider?.notes || provider?.service_notes || provider?.professional_headline || "";
+  const description =
+    provider?.notes ||
+    provider?.service_notes ||
+    provider?.professional_headline ||
+    "";
 
-  // resolve logo: provider.image may be a path or URL. If it's null, use defaultLogo
-  const logo = provider?.image ? provider.image : defaultLogo;
+  let logo = defaultLogo;
+  if (provider?.image) {
+    if (/^https?:\/\//i.test(provider.image)) {
+      logo = provider.image;
+    } else {
+      const cleanBase = IMG_URL.replace(/\/+$/, ""); 
+      const cleanPath = provider.image.replace(/^\/+/, "");
+      logo = `${cleanBase}/${cleanPath}`;
+    }
+  }
 
   return (
     <div className="detail-page-intro">
