13519c9e406fc1c6c1a7b07cfc81ec47b178bced
supportdotwibe <support@dotwibe.com>
2025-11-10 18:44:59 +0530

    up

--- src/app/(directory)/service-provider-directory/Components/AboutContainer.tsx
+++ src/app/(directory)/service-provider-directory/Components/AboutContainer.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
-const AboutContainer = ({ provider }) => {
-  const about = provider?.notes || provider?.description || "";
+const AboutContainer = () => {
   return (
-    <div className='about-content'>
-      <div className="">
-        <h3>About</h3>
-        <p>{about || "No about information provided."}</p>
+    <div className='about-content'> 
 
-        {provider?.extra_about && <p>{provider.extra_about}</p>}
+        <div className="">
+
+        <h3>About</h3>
+        <p>American Home Improvement, Inc. - it is our mission to provide the highest quality of service in all aspects of our business. We are extremely thorough in the services that we provide and aim to be very receptive to any client’s issues, questions or concerns and handle them promptly and professionally.</p>
+
+
+        <p>We take the necessary steps to ensure that our clients are completely satisfied with all of our contractual and assumed responsibilities. Above all else, we will fulfill these responsibilities while maintaining the highest ethical standards in both our work and our character.</p>
 
         <button className='show-more'>Show More</button>
-      </div>
+
+        </div>
+        
+      
     </div>
   )
 }
