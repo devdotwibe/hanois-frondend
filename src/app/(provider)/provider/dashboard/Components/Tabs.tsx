aebc3867088dc5972ea29d55265437d6bce4ac31
supportdotwibe <support@dotwibe.com>
2025-11-10 20:08:51 +0530

    a

--- src/app/(provider)/provider/dashboard/Components/Tabs.tsx
+++ src/app/(provider)/provider/dashboard/Components/Tabs.tsx
@@ -367,9 +367,8 @@ useEffect(() => {
       </ul>
      
       {/* Tab Content */}
-              <div className={`tab-panel ${activeTab === "companyinfo" ? "show" : ""}`}>
-
       <div className="tab-content-wrap">
+        <div className={`tab-panel ${activeTab === "companyinfo" ? "show" : ""}`}>
           <form className="settingsform company-profile1" onSubmit={handleSubmit}>
 
             <div className="form-grp">
