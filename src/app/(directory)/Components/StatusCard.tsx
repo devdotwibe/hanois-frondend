dfcaaefda1f4eecafbc1541838fa629074dea13f
supportdotwibe <support@dotwibe.com>
2025-11-10 19:24:44 +0530

    r

--- src/app/(directory)/Components/StatusCard.tsx
+++ src/app/(directory)/Components/StatusCard.tsx
@@ -5,8 +5,12 @@ const StatusCard = () => {
      <div className="status-card">
                 <div className="project-card">
 
+
+
+
       <button className="send-btn">Edit</button>
 
+      <button className="add-btn">Add New Project</button>
     </div>
 
             </div>
