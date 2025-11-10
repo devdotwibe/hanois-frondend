aefd10b53f7fe4f7dd7e0f5229715c174f88945e
supportdotwibe <support@dotwibe.com>
2025-11-10 12:03:46 +0530

    up

---
+++ src/app/(provider)/provider/dashboard/(pages)/add-project/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import uploadIcon from "../../../../../../public/images/upload.svg"; 
+
+const UploadBox = () => {
+  return (
+    <div className="upload-box">
+      <div className="upload-content">
+        {/* ✅ Replace UploadCloud with your image */}
+        <Image 
+          src={uploadIcon} 
+          alt="Upload Icon" 
+          width={40} 
+          height={40} 
+          className="upload-icon"
+        />
+
+        <h3>Upload an image</h3>
+        <p>Browse your files To upload Document</p>
+        <span>Supported Formats: JPEG, PNG</span>
+      </div>
+
+      <input type="file" accept="image/*" className="upload-input hidden" />
+    </div>
+  );
+};
+
+export default UploadBox;
