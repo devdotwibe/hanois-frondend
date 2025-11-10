2ab3d88198f74536cbc13b32d72f4cd07974a84d
supportdotwibe <support@dotwibe.com>
2025-11-10 12:04:21 +0530

    up

--- src/app/(provider)/provider/dashboard/(pages)/add-project/page.tsx
+++ src/app/(provider)/provider/dashboard/(pages)/add-project/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import uploadIcon from "../../../../../../public/images/upload.svg"; 
+import uploadIcon from "../../../../../../../public/images/upload.svg"; 
 
 const UploadBox = () => {
   return (
