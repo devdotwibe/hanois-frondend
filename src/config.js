d15a244a8e7537a417c90207ac5f20f1830a132f
supportdotwibe <support@dotwibe.com>
2025-11-11 15:17:40 +0530

    UP

--- src/config.js
+++ src/config.js
@@ -1,16 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hanois.dotwibe.com',
+        pathname: '/api/**',
+      },
+    ],
+  },
+};
 
+// Constants
 const API_URL = 'https://hanois.dotwibe.com/api/api/';
-
 const IMG_URL = 'https://hanois.dotwibe.com/api/';
-
-
 const SITE_URL = 'https://hanois.dotwibe.com/';
 // const API_URL = 'http://localhost:5000/api/';
 
-
-
-
-export {
+// Export both config and constants
+module.exports = {
+  ...nextConfig,
   API_URL,
   IMG_URL,
   SITE_URL,
