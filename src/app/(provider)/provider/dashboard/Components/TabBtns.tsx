b2950778f001fc5e327f859fd6022a2826cf4fe8
supportdotwibe <support@dotwibe.com>
2025-11-11 11:37:56 +0530

    up

--- src/app/(provider)/provider/dashboard/Components/TabBtns.tsx
+++ src/app/(provider)/provider/dashboard/Components/TabBtns.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const TabBtns = () => {
   return (
     <div className='proj-top'>
-
-        <ul className="tab-nav1">
-            <li><button className="tab-btn">Company Information</button></li>
-            <li><button className="tab-btn active">Project</button></li>
-            </ul>
-      
+      <ul className="tab-nav1">
+        <li>
+          <button
+            className="tab-btn"
+            onClick={() => window.location.href = 'https://hanois.dotwibe.com/provider/dashboard/company-profile'}
+          >
+            Company Information
+          </button>
+        </li>
+        <li>
+          <button className="tab-btn active">
+            Project
+          </button>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default TabBtns
+export default TabBtns;
