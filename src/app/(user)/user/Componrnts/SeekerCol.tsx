6b56a7115306c8e26794eaef3acb43cb59cf8517
abindotwibe <abindotwibe@outlook.com>
2025-11-11 17:52:28 +0530

    up

--- src/app/(user)/user/Componrnts/SeekerCol.tsx
+++ src/app/(user)/user/Componrnts/SeekerCol.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import { useRouter } from "next/navigation";
 
 const SeekerCol = () => {
+  const router = useRouter();
+
+  const goToAddProject = () => {
+    router.push("/user/add-project");
+  };
+
   return (
-      <div className="seeker-col1">
+    <div className="seeker-col1">
 
+      <div className="proj-text">
+        <h2>My Project</h2>
+        <p>Here is the list of your projects, you can follow all updates of them</p>
+      </div>
 
+      <button
+        className='new-proj'
+        onClick={goToAddProject}
+      >
+        Add New Project
+      </button>
 
-
-          <div className="proj-text">
-            <h2>My Project</h2>
-            <p>Here is the list of your projects, you can follow all updates of them</p>
-          </div>
-
-
-
-
-          <button className='new-proj'>Add New Project</button>
-
-      
-
-        </div>
+    </div>
   )
 }
 
-export default SeekerCol
+export default SeekerCol;
