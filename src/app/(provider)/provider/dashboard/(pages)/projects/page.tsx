704a0c671e464a0d102407c5a410fcf48e91dc9f
supportdotwibe <support@dotwibe.com>
2025-11-12 16:56:50 +0530

    aa

--- src/app/(provider)/provider/dashboard/(pages)/projects/page.tsx
+++ src/app/(provider)/provider/dashboard/(pages)/projects/page.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { API_URL, IMG_URL, SITE_URL } from "@/config";
-import DetailCard from "@/app/(directory)/provider/Components/DetailCard";
+import HouseOuter from "../../Components/HouseOuter";
 import TabBtns from "../../Components/TabBtns";
 import TorranceCard from "../../Components/TorranceCard";
 import UploadBox from "../../Components/UploadBox";
@@ -58,11 +58,7 @@ const ProjectComponent = () => {
 
   return (
     <div className="project-component">
-      <DetailCard
-        logo="/path/to/logo.png" // Placeholder logo, replace with actual project logo if available
-        name="Project Name" // Placeholder project name, replace with actual project name
-        description="This is a detailed description of the project." // Optional project description
-      />
+      <HouseOuter />
       <TabBtns />
 
       {/* 🟩 Add Button */}
