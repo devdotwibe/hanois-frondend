5fbe1a6aa2a88936bb4037c69cf0b7e266b893a1
supportdotwibe <support@dotwibe.com>
2025-11-12 19:38:03 +0530

    up

---
+++ src/app/(directory)/companydetail/page.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import AboutContainer from '../../Components/AboutContainer'
+import DetailIntro from '@/app/(directory)/provider/Components/DetailIntro'
+import ItemSlider from '@/app/(provider)/provider/dashboard/Components/ItemSlider'
+import BusinessInfo from '@/app/(directory)/provider/Components/BusinessInfo'
+import ServiceDiv from '../../Components/ServiceDiv'
+import TorranceSlider from '../../Components/TorranceSlider'
+import StatusCard from '@/app/(directory)/provider/Components/StatusCard'
+
+
+const page = () => {
+  return (
+    <div className='detailpage'>
+
+        <div className="containers-limit detcol">
+
+          <div className="detcol-1">
+
+         <DetailIntro />
+       
+         <AboutContainer />
+
+         <BusinessInfo />
+
+         <ServiceDiv />
+
+
+         
+      
+
+        </div>
+
+        <div className="detcol-2 after-edit">
+          <StatusCard />
+        </div>
+
+
+
+        </div>
+
+
+
+
+
+        <div className="containers-limit">
+            <TorranceSlider />
+
+        </div>
+
+
+        
+
+
+       
+
+    
+    </div>
+  )
+}
+
+export default page
