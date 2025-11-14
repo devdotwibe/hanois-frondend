af8159a0b048f50ce7dda1d1096f3b9ee5e9cb04
sreeparvathy13 <sreeparvathydotwibe@outlook.com>
2025-11-14 16:08:37 +0530

    ss

--- src/app/(provider)/provider/dashboard/page.tsx
+++ src/app/(provider)/provider/dashboard/page.tsx
@@ -2,6 +2,11 @@
 
 import { API_URL } from "@/config";
 import React, { useState,useEffect } from 'react'
+import { createPortal } from "react-dom";
+import Image from "next/image";
+import img1 from "../../../../../public/images/profile.png"
+import Link from "next/link";
+
 
 const leads = [
   {
@@ -75,6 +80,8 @@ const getStatusColor = (status) => {
 
 
 const Page = () => {
+
+  const [openPopup, setOpenPopup] = useState(false);
 
 
   const [leads, setLeads] = useState([]);
@@ -179,15 +186,196 @@ const Page = () => {
                   <td className="date-col">{new Date(lead.created_at).toLocaleDateString('en-GB')}</td>
                   <td className="email-col">{lead?.user?.email}</td>
                   <td className="mob-col">{lead?.phone?.mobile}</td>
-                  <td className="status-col">
+
+
+                  {/* <td className="status-col">
                     <span
                       className={`highlightedd bg-blue ${getStatusColor(
                         lead.status
-                      )}`}
+
+                        
+                      )}`
+
+                      
+                    
+                    }
                     >
                       {lead.status}
                     </span>
-                  </td>
+                  </td> */}
+
+
+                  <td className="status-col">
+        <span
+          className={`highlightedd bg-blue ${getStatusColor(lead.status)}`}
+          onClick={() => setOpenPopup(true)}
+          style={{ cursor: "pointer" }}
+        >
+          {lead.status}
+        </span>
+      </td>
+
+
+
+       {openPopup &&
+       createPortal(
+
+        <div className="modal-overlay proposal-popup lead-popup" onClick={() => setShowPopup(false)}>
+                  <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <button className="close-btn" onClick={() => setShowPopup(false)}>
+                </button>
+            <div className="proposal-box">
+              {/* Header Section */}
+              <div className="proposal-header">
+                <Image
+                  src={img1}
+                  alt="Nilson Todd"
+                  width={70}
+                  height={70}
+                  className="proposal-logo"
+                />
+                <div className="proposal-info">
+                  <h3>Nilson Todd</h3>
+                  <p>nillson.ni@gmaim.com</p>
+                  <p>+1 (866) 580-2168</p>
+                </div>
+              </div>
+        
+              {/* Details Section */}
+              <div className="proposal-details lead-details">
+
+                <h4>Project Details<span>Private</span> </h4>
+
+                <h5>Building a house from the scratch</h5>
+                <h5>About the project:</h5>
+
+                <div className="cont-lead">
+                  <p>American Home Improvement, Inc. – it is our mission to provide the highest quality of service in all aspects of our business. We are extremely thorough in the services that we provide and aim to be very receptive to any client’s issues, questions or concerns and handle them promptly and professionally.</p>
+                </div>
+
+
+
+
+                <div className="detail-row">
+        
+                  <div className="detail-col11">
+                    <p><span>Type</span></p>
+                  </div>
+                  <div className="detail-col11">
+                       <p>Housing</p>
+                  </div>
+                </div>
+        
+                <div className="detail-row">
+                    <div className="detail-col11">
+                    <p><span>Location</span></p>
+                  </div>
+                  <div className="detail-col11">
+                    <p>New York</p>
+                  </div>
+        
+                </div>
+        
+                <div className="detail-row">
+                    <div className="detail-col11">
+                    <p><span>Land size</span></p>
+                  </div>
+                  <div className="detail-col11">
+                       <p>56 m2</p>
+                  </div>
+                </div>
+        
+                <div className="detail-row">
+                    <div className="detail-col11">
+                    <p><span>Luxury level</span></p>
+                  </div>
+                  <div className="detail-col11">
+                       <p>Basic</p>
+                  </div>
+                </div>
+        
+                <div className="detail-row">
+                    <div className="detail-col11">
+                    <p><span>Basement</span></p>
+                  </div>
+                  <div className="detail-col11">
+                       <p>Basement</p>
+                  </div>
+                </div>
+
+
+
+
+
+                <form>
+
+                  <div className="form-grp">
+                    <label>status</label>
+                    <select></select>
+                  </div>
+
+
+                  
+                  <div className="form-grp">
+                    <label>Notes</label>
+                    <textarea placeholder="Proposal Letters" rows={4}></textarea>
+                    <small>Breif discription for your profile, URLs are hyperlinked </small>
+                  </div>
+
+
+
+
+
+
+
+                </form>
+        
+
+
+        
+        
+                
+        
+                
+              </div>
+
+
+
+
+              <div className="proposal-actions">
+                <button className="cancel-btn1">Cancel</button>
+                <button className="accept-btn">Save</button></div>
+        
+           
+            </div>
+        
+                  </div>
+                </div>
+                 ,
+                document.body
+
+
+
+       
+
+
+
+
+      )}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 </tr>
               ))}
           </tbody>
