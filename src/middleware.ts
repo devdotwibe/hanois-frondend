1c56ac69e45170460da750ee7387b9abe77f1685
ArjunlalDotwibe <arjund@twibe@3>
2025-11-19 20:06:45 +0530

    up

--- src/middleware.ts
+++ src/middleware.ts
@@ -122,7 +122,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-      "/((?!_next/static|_next/image|favicon.ico).*)",
+     "/((?!_next/static|_next/image|favicon.ico|api|strapi).*)",
       "/admin/:path*",
       "/users/:path*",
     ],
