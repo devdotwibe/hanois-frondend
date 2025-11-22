75101c3f707f341eda2b236d1b88f46ad5261695
supportdotwibe <support@dotwibe.com>
2025-11-22 17:35:03 +0530

    up

---
+++ src/app/(user)/user/project/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import SeekerCol from '../Componrnts/SeekerCol'
+import RemodellingDiv from '../Componrnts/RemodellingDiv'
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <div className="seeker-div">
+
+
+
+
+        <SeekerCol />
+
+       
+
+
+     
+
+
+      </div>
+
+
+       <RemodellingDiv />
+      
+    </div>
+  )
+}
+
+export default UserDashboard
